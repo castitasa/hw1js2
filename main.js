c505691015134affd082fc9e2e5d3cee7beb906c
castitasa <aibike0806@gmail.com>
2023-09-29 20:40:40 +0600

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-
-let nam = prompt ('Вы успешно зарегистрировались!')
 const inputEmail = document.getElementById('email')
 const button = document.getElementById('button')
+const error = document.getElementById('error') 
 
-
-button.onclick = () =>{
+button.onclick = (e) =>{
+    e.preventDefault()
     const g =/\w+@\w+\.\w+/
     console.log(g.test(inputEmail.value));
+    if (g.test(inputEmail.value)) {
+        
+        mistake.textContent= ''
+        alert ('Вы успешно зарегистрировались');
+    } else {
+        mistake.textContent= 'не корректный формат почты';
+    }
 }
-
-
